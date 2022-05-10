1a5f999053639ac72e183b2d60458dbdf2beba25
efojug <lidudu890890@163.com>
2022-05-10 11:35:45 +0800

    first

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Panuon.UI.Silver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,55 @@ namespace Grasssummoner
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowX
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectGame_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void SelectProxy_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void SelectGrasscutter_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void saveconfig_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void loadconfig_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void OnlyStartMitmproxy_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void OnlyStartServer_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void OnlyStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+        private void StartAll_Click(object sender, RoutedEventArgs e)
+        {
+            return;
         }
     }
 }
